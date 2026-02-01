0ff1b201cad718e9f0be4c021781d53eb83095cc
guilherme.guia <guilhermeguia007@gmail.com>
2026-02-01 13:46:41 -0400

    Feat: Dbcontext

---
+++ DesafioSpedy.Infrastructure/DataAccess/DesafioSpedyDbContext.cs
@@ -0,0 +1,18 @@
+﻿using DesafioSpedy.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DesafioSpedy.Infrastructure.DataAccess;
+
+public class DesafioSpedyDbContext : DbContext
+{
+    public DesafioSpedyDbContext(DbContextOptions<DesafioSpedyDbContext> options) : base(options) { }
+
+    #region DBSETS
+        public virtual DbSet<User> User { get; set; }
+    #endregion
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(DesafioSpedyDbContext).Assembly);
+    }
+}
---
+++ DesafioSpedy.Infrastructure/Migrations/20260201165241_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DesafioSpedy.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdateAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DeleteAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
