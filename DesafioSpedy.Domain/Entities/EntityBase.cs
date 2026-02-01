a4d8b5033040a129b5c3f04b45de5fdbe6dbbd36
guilherme.guia <guilhermeguia007@gmail.com>
2026-02-01 11:34:58 -0400

    Feat: adicionando entitidade usuario

---
+++ DesafioSpedy.Domain/Entities/EntityBase.cs
@@ -0,0 +1,9 @@
+﻿namespace DesafioSpedy.Domain.Entities;
+
+public class EntityBase
+{
+    public Guid Id { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdateAt { get; set; }
+    public DateTime DeleteAt { get; set; }
+}
