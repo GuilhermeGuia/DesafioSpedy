b3a10dc20d625a44cb019af53561e42c8b09e93f
guilherme.guia <guilhermeguia007@gmail.com>
2026-02-01 11:28:03 -0400

    Feat: adicionando login dto

---
+++ DesafioSpedy.Application/Dtos/Auth/CredentialsDto.cs
@@ -0,0 +1,7 @@
+﻿namespace DesafioSpedy.Application.Dtos.Auth;
+
+public class CredentialsDto
+{
+    public string Email = string.Empty;
+    public string Password = string.Empty;
+}
---
+++ DesafioSpedy.Application/Dtos/Auth/LoginResponseDto.cs.cs
@@ -0,0 +1,6 @@
+﻿namespace DesafioSpedy.Application.Dtos.Auth;
+
+public class LoginResponseDto
+{
+    public string Token { get; set; } = string.Empty;
+}
