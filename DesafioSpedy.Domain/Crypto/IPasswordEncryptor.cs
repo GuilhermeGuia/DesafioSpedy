d4235980f371ef625db8657991b1cf71f7a8ffd6
guilherme.guia <guilhermeguia007@gmail.com>
2026-02-01 13:47:18 -0400

    Feat: Adicionando gerador de senha

---
+++ DesafioSpedy.Domain/Crypto/IPasswordEncryptor.cs
@@ -0,0 +1,7 @@
+﻿namespace DesafioSpedy.Domain.Crypto;
+
+public interface IPasswordEncryptor
+{
+    string Hash(string plainPassword);
+    bool Verify(string plainPassword, string hashedPassword);
+}
---
+++ DesafioSpedy.Infrastructure/Crypto/PasswordEncryptor.cs
@@ -0,0 +1,20 @@
+﻿using DesafioSpedy.Domain.Crypto;
+using Microsoft.AspNetCore.Identity;
+
+namespace DesafioSpedy.Infrastructure.Crypto;
+
+public class PasswordEncryptor : IPasswordEncryptor
+{
+    private readonly PasswordHasher<object> _hasher = new();
+
+    public string Hash(string plainPassword)
+    {
+        return _hasher.HashPassword(null, plainPassword);
+    }
+
+    public bool Verify(string plainPassword, string hashedPassword)
+    {
+        var result = _hasher.VerifyHashedPassword(null, hashedPassword, plainPassword);
+        return result == PasswordVerificationResult.Success;
+    }
+}
