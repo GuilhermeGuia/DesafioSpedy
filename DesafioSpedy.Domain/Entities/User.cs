bacdd379aaa4e834156eb9c3707ff52cc125636c
guilherme.guia <guilhermeguia007@gmail.com>
2026-02-01 13:49:03 -0400

    Feat: Entity user

--- DesafioSpedy.Domain/Entities/User.cs
+++ DesafioSpedy.Domain/Entities/User.cs
@@ -1,8 +1,18 @@
-﻿namespace DesafioSpedy.Domain.Entities;
+﻿using DesafioSpedy.Domain.Crypto;
+
+namespace DesafioSpedy.Domain.Entities;
 
 public class User : EntityBase
 {
-    public string Name { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
+    public string Name { get; private set; } = string.Empty;
+    public string Email { get; private set; } = string.Empty;
+    public string Password { get; private set; } = string.Empty;
+
+    public void ValidarLogin(string password, IPasswordEncryptor encryptor)
+    {
+        if (!encryptor.Verify(password, Password))
+            throw new Exception("Email ou Senha incorretos.");
+            //throw new CredenciaisInvalidasException();
+    }
+
 }
