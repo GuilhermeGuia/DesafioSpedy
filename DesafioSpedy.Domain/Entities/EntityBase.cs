734fe5459436e01d0f5de2cbe96bccc057f9a2f9
guilherme.guia <guilhermeguia007@gmail.com>
2026-02-02 21:40:19 -0400

    Fix: ajustando campo deleteAt

--- DesafioSpedy.Domain/Entities/EntityBase.cs
+++ DesafioSpedy.Domain/Entities/EntityBase.cs
@@ -5,5 +5,4 @@ public class EntityBase
     public Guid Id { get; protected set; }
     public DateTime CreatedAt { get; protected set; } = DateTime.UtcNow;
     public DateTime? UpdateAt { get; protected set; }
-    public DateTime? DeleteAt { get; protected set; }
 }
--- DesafioSpedy.Domain/Entities/User.cs
+++ DesafioSpedy.Domain/Entities/User.cs
@@ -14,4 +14,5 @@ public class User : EntityBase
     public string Name { get; private set; } = string.Empty;
     public string Email { get; private set; } = string.Empty;
     public string Password { get; private set; } = string.Empty;
+    public DateTime? DeleteAt { get; protected set; }
 }
