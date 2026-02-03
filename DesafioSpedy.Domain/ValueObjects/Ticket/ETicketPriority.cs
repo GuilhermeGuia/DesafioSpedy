54157fdd816e765ba45ecd5b2c282f5307ca6d3d
guilherme.guia <guilhermeguia007@gmail.com>
2026-02-02 21:39:41 -0400

    Feat: adicionando value objects status e prioridade

---
+++ DesafioSpedy.Domain/ValueObjects/Ticket/ETicketPriority.cs
@@ -0,0 +1,8 @@
+﻿namespace DesafioSpedy.Domain.ValueObjects.Ticket;
+
+public enum ETicketPriority
+{
+    Low = 1,
+    Medium = 2,
+    High = 3
+}
---
+++ DesafioSpedy.Domain/ValueObjects/Ticket/ETicketStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace DesafioSpedy.Domain.ValueObjects.Ticket;
+
+public enum ETicketStatus
+{
+    Open = 1,
+    InProgress = 2,
+    Resolved = 3,
+    Closed = 4
+}
