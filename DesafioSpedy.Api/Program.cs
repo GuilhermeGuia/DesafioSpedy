466ec802397b799bf4791277ce39bebd713176fe
guilherme.guia <guilhermeguia007@gmail.com>
2026-02-02 21:04:03 -0400

    Feat: Adicionando migrate e seed

--- DesafioSpedy.Api/Program.cs
+++ DesafioSpedy.Api/Program.cs
@@ -1,8 +1,9 @@
 using DesafioSpedy.Api.Filters;
 using DesafioSpedy.Application;
+using DesafioSpedy.Domain.Crypto;
 using DesafioSpedy.Infrastructure;
 using DesafioSpedy.Infrastructure.DataAccess;
-using FluentValidation;
+using DesafioSpedy.Infrastructure.Poco;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,12 +12,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly, includeInternalTypes: true);
 builder.Services.AddMvc(options =>
 {
     options.Filters.Add<ResponseFilter>();
     options.Filters.Add<ExceptionGlobalFilter>();
 });
+
+builder.Services.Configure<JwtSettings>(
+    builder.Configuration.GetSection("JwtSettings"));
 
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
@@ -29,16 +32,24 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<DesafioSpedyDbContext>();
-    db.Database.Migrate();
-}
-
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
 
+MigrateAndSeedDatabaseAsync();
+
 app.Run();
+
+void MigrateAndSeedDatabaseAsync()
+{
+    using var scope = app.Services.CreateScope();
+    var services = scope.ServiceProvider;
+
+    var db = services.GetRequiredService<DesafioSpedyDbContext>();
+    var passwordEncryptor = services.GetRequiredService<IPasswordEncryptor>();
+
+    db.Database.MigrateAsync();
+    DbInitializer.Seed(db, passwordEncryptor);
+}
---
+++ DesafioSpedy.Infrastructure/DataAccess/DbInitializer.cs
@@ -0,0 +1,25 @@
+﻿using DesafioSpedy.Domain.Crypto;
+using DesafioSpedy.Domain.Entities;
+
+namespace DesafioSpedy.Infrastructure.DataAccess;
+
+public static class DbInitializer
+{
+    public static void Seed(
+            DesafioSpedyDbContext _db,
+            IPasswordEncryptor passwordEncryptor)
+    {
+        if (_db.User.Any())
+            return;
+
+        var adminUser = new User(
+            id: Guid.NewGuid(),
+            name: "Admin",
+            email: "admin@admin.com",
+            passwordHash: passwordEncryptor.Hash("123456")
+        );
+
+        _db.User.Add(adminUser);
+        _db.SaveChanges();
+    }
+}
